691ec39cedeb501fd2c028f45c1c7528d66ef107
duyi0518 <423403391@qq.com>
2017-12-18 23:30:02 +0800

    duyi1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React,{Component} from 'react';
 import Tab from "./components/Tab/index";
+import NavHeader from "./components/NavHeader/index";
+import BackD from "./container/BackD/index";
 import {Route} from 'react-router-dom';
 import history from './store/history'
 import {ConnectedRouter} from 'react-router-redux';
 import './style/comm.less'
+import InforD from "./container/InforD/index";
 export default class App extends Component{
     render(){
         return (
             <ConnectedRouter history={history}>
                 <div>
+                    <NavHeader/>
+                    <InforD/>
                     <Tab/>
                 </div>
             </ConnectedRouter>
