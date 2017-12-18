10ebfb23355535ff645456e8bcde4fc392ba36dc
gaoxing88 <943016287@qq.com>
2017-12-18 23:13:31 +0800

    update component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import history from './store/history'
 import {ConnectedRouter} from 'react-router-redux';
 import './style/comm.less'
 import Home from "./container/Home/index";
+import Profile from "./container/Profile/index";
+import Login from "./container/Login/index";
+import Reg from "./container/Reg/index";
+import Address from "./container/Address/index";
+import AddAddress from "./container/AddAddress/index";
 export default class App extends Component{
     render(){
         return (
@@ -12,6 +17,11 @@ export default class App extends Component{
                 <div>
                     <Tab/>
                     <Route exact  path="/" component={Home}/>
+                    <Route exact  path="/profile" component={Profile}/>
+                    <Route exact  path="/login" component={Login}/>
+                    <Route exact  path="/reg" component={Reg}/>
+                    <Route exact  path="/address" component={Address}/>
+                    <Route exact  path="/addadress" component={AddAddress}/>
                 </div>
             </ConnectedRouter>
         )
